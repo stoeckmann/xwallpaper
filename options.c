10de6c73acfda5057f7fb596d1a196245a9e2d6c
Tobias Stoeckmann <tobias@stoeckmann.org>
2017-03-27 19:41:44 +0200

    Added optional RandR support.
    A wallpaper program without RandR support is a real chore when used with
    multiple monitors. If anybody wants to debate about RandR vs. Xinerama
    in the year 2017, here is my rationale for choosing RandR:
    
    - Xinerama has no possibility of naming an output screen. This alone
      makes it really difficult for the user to get things right.
    - RandR is well supported by now. In fact, Xinerama is legacy.
    - By using RandR I was able to shamelessly take over the xrandr syntax
      for xsetwallpaper. ;) Keeping things in sync is always a good idea.
    
    Obviously this step means a bump in version, therefore 0.2.0.
    
    Signed-off-by: Tobias Stoeckmann <tobias@stoeckmann.org>

---
+++ options.c
@@ -0,0 +1,196 @@
+/*
+ * Copyright (c) 2017 Tobias Stoeckmann <tobias@stoeckmann.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <err.h>
+#include <limits.h>
+#include <pixman.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "config.h"
+#include "functions.h"
+
+static size_t
+add_buffer(wp_buffer_t **bufs, size_t *count, wp_buffer_t buf)
+{
+	size_t i;
+
+	for (i = 0; i < *count; i++)
+		if ((*bufs)[i].st_dev == buf.st_dev &&
+		    (*bufs)[i].st_ino == buf.st_ino)
+			break;
+
+	if (*count != 0 && i != *count)
+		fclose(buf.fp);
+	else {
+		*bufs = realloc(*bufs, (*count + 1) * sizeof(**bufs));
+		if (*bufs == NULL)
+			err(1, "failed to allocate memory");
+		memcpy(&(*bufs)[(*count)++], &buf, sizeof(buf));
+	}
+
+	return i;
+}
+
+static void
+add_option(wp_option_t **opts, size_t *count, wp_option_t opt)
+{
+	size_t i;
+	wp_option_t *o;
+
+	if (opt.filename == NULL)
+		return;
+
+	for (i = 0; i < *count; i++)
+		if (strcmp((*opts)[i].output, opt.output) == 0 &&
+		    (*opts)[i].screen == opt.screen)
+			break;
+
+	if (*count != 0 && i != *count)
+		o = &(*opts)[i];
+	else {
+		*opts = realloc(*opts, (*count + 2) * sizeof(**opts));
+		if (*opts == NULL)
+			err(1, "failed to allocate memory");
+		o = &(*opts)[(*count)++];
+		(*opts)[(*count)].filename = NULL;
+	}
+
+	memcpy(o, &opt, sizeof(opt));
+}
+
+static void
+init_buffers(wp_option_t *options, size_t options_count)
+{
+	wp_buffer_t *buffers, buffer;
+	size_t buffers_count, i;
+	size_t *refs;
+
+	refs = calloc(options_count, sizeof(*refs));
+	if (refs == NULL)
+		err(1, "failed to allocate memory");
+
+	buffers = NULL;
+	buffers_count = 0;
+	memset(&buffer, 0, sizeof(buffer));
+
+	for (i = 0; i < options_count; i++) {
+		struct stat st;
+
+		if ((buffer.fp = fopen(options[i].filename, "r")) == NULL)
+			err(1, "failed to open %s", options[i].filename);
+		if (fstat(fileno(buffer.fp), &st))
+			err(1, "failed to stat %s", options[i].filename);
+		buffer.st_dev = st.st_dev;
+		buffer.st_ino = st.st_ino;
+
+		refs[i] = add_buffer(&buffers, &buffers_count, buffer);
+	}
+
+	for (i = 0; i < options_count; i++)
+		options[i].buffer = &buffers[refs[i]];
+	free(refs);
+}
+
+static int
+parse_mode(char *mode)
+{
+	if (strncmp(mode, "--", 2))
+		return -1;
+	mode += 2;
+
+	if (strcmp(mode, "center") == 0)
+		return MODE_CENTER;
+	if (strcmp(mode, "maximize") == 0)
+		return MODE_MAXIMIZE;
+	if (strcmp(mode, "stretch") == 0)
+		return MODE_STRETCH;
+	if (strcmp(mode, "tile") == 0)
+		return MODE_TILE;
+	if (strcmp(mode, "zoom") == 0)
+		return MODE_ZOOM;
+	return -1;
+}
+
+static int
+parse_screen(char *screen)
+{
+	char *endptr;
+	long int value;
+
+	value = strtol(screen, &endptr, 10);
+	if (endptr == screen || *endptr != '\0' || value < 0 || value > INT_MAX)
+		errx(1, "failed to parse screen number: %s", optarg);
+	return value;
+}
+
+wp_option_t *
+parse_options(char **argv)
+{
+	wp_option_t *options, last;
+	size_t count;
+
+	options = NULL;
+	count = 0;
+
+	memset(&last, 0, sizeof(last));
+	last.screen = -1;
+
+	while (*argv != NULL) {
+		if (strcmp(argv[0], "--screen") == 0) {
+			if (*++argv == NULL) {
+				warnx("missing argument for --screen");
+				return NULL;
+			}
+			last.screen = parse_screen(*argv);
+		} else if (strcmp(argv[0], "--output") == 0) {
+			if (*++argv == NULL) {
+				warnx("missing argument for --output");
+				return NULL;
+			}
+#ifdef WITH_RANDR
+			add_option(&options, &count, last);
+			last.output = *argv;
+#else
+			warnx("RandR support is missing, ignoring --output");
+#endif /* WITH_RANDR */
+		} else if ((last.mode = parse_mode(*argv)) != -1) {
+			if (*++argv == NULL) {
+				warnx("missing argument for %s", *argv);
+				return NULL;
+			}
+			last.filename = *argv;
+		} else if (strcmp(argv[0], "--version") == 0) {
+			puts(VERSION);
+			exit(0);
+		} else {
+			warnx("illegal argument: %s", *argv);
+			return NULL;
+		}
+		++argv;
+	}
+	add_option(&options, &count, last);
+
+	init_buffers(options, count);
+
+	return options;
+}
+
---
+++ outputs.c
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2017 Tobias Stoeckmann <tobias@stoeckmann.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include "config.h"
+
+#include <xcb/xcb.h>
+#ifdef WITH_RANDR
+  #include <xcb/randr.h>
+#endif
+
+#include <err.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "functions.h"
+
+wp_output_t *
+get_output(wp_output_t *outputs, char *name)
+{
+	wp_output_t *output;
+
+	for (output = outputs; output->name != NULL; output++)
+		if (name != NULL && strcmp(output->name, name) == 0)
+			return output;
+
+	return output;
+}
+
+wp_output_t *
+get_outputs(xcb_connection_t *c, xcb_screen_t *screen)
+{
+	wp_output_t *outputs;
+	int len;
+#ifdef WITH_RANDR
+	xcb_randr_get_screen_resources_cookie_t resources_cookie;
+	xcb_randr_get_screen_resources_reply_t *resources_reply;
+	xcb_randr_output_t *xcb_outputs;
+	int i;
+
+	resources_cookie = xcb_randr_get_screen_resources(c, screen->root);
+	resources_reply = xcb_randr_get_screen_resources_reply(c,
+	    resources_cookie, NULL);
+
+	xcb_outputs = xcb_randr_get_screen_resources_outputs(resources_reply);
+	len = xcb_randr_get_screen_resources_outputs_length(resources_reply);
+	if (len < 1)
+		errx(1, "failed to retrieve randr outputs");
+
+	outputs = calloc(len + 1, sizeof(*outputs));
+	if (outputs == NULL)
+		errx(1, "failed to allocate memory");
+
+	for (i = 0; i < len; i++) {
+		xcb_randr_get_output_info_cookie_t output_cookie;
+		xcb_randr_get_output_info_reply_t *output_reply;
+		xcb_randr_get_crtc_info_cookie_t crtc_cookie;
+		xcb_randr_get_crtc_info_reply_t *crtc_reply;
+		xcb_randr_crtc_t *crtc;
+		int crtc_len, name_len;
+		uint8_t *name;
+
+		output_cookie = xcb_randr_get_output_info(c, xcb_outputs[i],
+		    XCB_CURRENT_TIME);
+		output_reply = xcb_randr_get_output_info_reply(c, output_cookie,
+		    NULL);
+
+		name = xcb_randr_get_output_info_name(output_reply);
+		name_len = xcb_randr_get_output_info_name_length(output_reply);
+
+		crtc = xcb_randr_get_output_info_crtcs(output_reply);
+		crtc_len = xcb_randr_get_output_info_crtcs_length(output_reply);
+
+		if ((outputs[i].name = malloc(name_len + 1)) == NULL)
+			errx(1, "failed to allocate memory");
+		memcpy(outputs[i].name, name, name_len);
+		outputs[i].name[name_len] = '\0';
+
+		if (crtc_len < 1)
+			errx(1, "failed to retrieve CRTCs for output %s",
+			    outputs[i].name);
+
+		crtc_cookie = xcb_randr_get_crtc_info(c, crtc[0],
+		    XCB_CURRENT_TIME);
+		crtc_reply = xcb_randr_get_crtc_info_reply(c, crtc_cookie,
+		    NULL);
+
+		outputs[i].x = crtc_reply->x;
+		outputs[i].y = crtc_reply->y;
+		outputs[i].width = crtc_reply->width;
+		outputs[i].height = crtc_reply->height;
+	}
+#else
+	outputs = malloc(sizeof(*outputs));
+	if (outputs == NULL)
+		errx(1, "failed to allocate memory");
+	len = 0;
+#endif /* WITH_RANDR */
+
+	outputs[len].name = NULL;
+	outputs[len].x = 0;
+	outputs[len].y = 0;
+	outputs[len].width = screen->width_in_pixels;
+	outputs[len].height = screen->height_in_pixels;
+
+	return outputs;
+}
+
