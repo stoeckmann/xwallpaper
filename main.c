3ba52be16a0ffbe4eacb5bd13dfcb1353173223a
Tobias Stoeckmann <tobias@stoeckmann.org>
2017-03-29 13:20:05 +0200

    Do not scale for centered images.
    There is no need to call pixman_f_transform_scale for centered images,
    because it's already known that the scaling factor is 1.
    
    Signed-off-by: Tobias Stoeckmann <tobias@stoeckmann.org>

--- main.c
+++ main.c
@@ -140,7 +140,8 @@ transform(pixman_image_t *dest, wp_output_t *output, wp_option_t *option)
 	pixman_f_transform_init_identity(&ftransform);
 	pixman_f_transform_translate(&ftransform, NULL,
 	    translate_x, translate_y);
-	pixman_f_transform_scale(&ftransform, NULL, w_scale, h_scale);
+	if (option->mode != MODE_CENTER)
+		pixman_f_transform_scale(&ftransform, NULL, w_scale, h_scale);
 
 	pixman_transform_from_pixman_f_transform(&transform, &ftransform);
 	pixman_image_set_transform(pixman_image, &transform);
