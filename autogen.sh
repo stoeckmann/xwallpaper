bf88b298652c7d01a72263fbb6d129ac5721d6b2
Tobias Stoeckmann <tobias@stoeckmann.org>
2018-06-02 21:53:33 +0200

    Verify that pkg-config exists on system.
    If pkg-config does not exist on system while running autogen.sh, the
    resulting configure script will most likely not work.
    
    The reason is that a used pkg-config macro will not expand if
    pkg.m4 is not found.
    
    Spotted and reported by Ted Moseley.
    
    Signed-off-by: Tobias Stoeckmann <tobias@stoeckmann.org>

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+# check if pkg-config is installed, otherwise ./configure will fail.
+pkg-config --version > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+	echo "Required package "pkg-config" not found."
+	exit 1
+fi
+
 autoreconf -fiv
