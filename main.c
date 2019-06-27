c5ea6cee9a59f2fd6a990b5d0fbd9f13c18f538f
Tobias Stoeckmann <tobias@stoeckmann.org>
2019-06-27 16:57:42 +0200

    Fix --focus yet again.
    Zooming into the image must only be allowed if the adjusted
    trimbox is larger than the input file. Basically this means that
    we never zoom in after zooming out and that sounds reasonable.
    
    Issue noticed and reported by Marc Espie, thanks again!
    
    Signed-off-by: Tobias Stoeckmann <tobias@stoeckmann.org>

--- main.c
+++ main.c
@@ -218,7 +218,7 @@ transform(pixman_image_t *dest, wp_output_t *output, wp_option_t *option)
 
 	if (mode == MODE_FOCUS) {
 		int adj_x, adj_y, adj_width, adj_height;
-		float zoom;
+		float height_zoom, width_zoom, zoom;
 
 		adj_width = src_width;
 		adj_height = src_height;
@@ -231,9 +231,20 @@ transform(pixman_image_t *dest, wp_output_t *output, wp_option_t *option)
 		adj_width = xcb_width * zoom;
 		adj_height = xcb_height * zoom;
 
-		/* Check if image is smaller than adjusted box. */
-		zoom = MAXIMUM(1, (float)adj_width / pix_width);
-		zoom = MAXIMUM(zoom, (float)adj_height / pix_height);
+		/*
+		 * Check if image is smaller than adjusted box.
+		 *
+		 * The adjusted box must fully contain the image, i.e.
+		 * adjusted width AND height must be larger than width
+		 * and height of the input image. Otherwise the trim box
+		 * will be zoomed into and the constraint is violated.
+		 */
+		width_zoom = (float)adj_width / pix_width;
+		height_zoom = (float)adj_height / pix_height;
+		if (width_zoom > 1 && height_zoom > 1)
+			zoom = MAXIMUM(width_zoom, height_zoom);
+		else
+			zoom = 1;
 
 		/* Zoom in only as much as necessary. */
 		adj_width = MAXIMUM(1, adj_width / zoom);
