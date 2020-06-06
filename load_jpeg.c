ec83a195671eee3b2c9f006f81ca8fed7ea990ca
Tobias Stoeckmann <tobias@stoeckmann.org>
2020-06-06 22:58:51 +0200

    Prevent undefined behaviour with setjmp.
    Avoid touching local variables which may have changed after setjmp
    call. This is in sync with libjpeg-turbo's example.txt where this
    code basically derived from.
    
    Signed-off-by: Tobias Stoeckmann <tobias@stoeckmann.org>

--- load_jpeg.c
+++ load_jpeg.c
@@ -41,55 +41,66 @@ error_jpg(j_common_ptr common)
 	longjmp(wp_err->env, 1);
 }
 
-pixman_image_t *
-load_jpeg(FILE *fp)
+static pixman_image_t *
+do_load_jpeg(FILE *fp, struct jpeg_decompress_struct *cinfo, uint32_t **pixels)
 {
 	wp_err_t wp_err;
 	pixman_image_t *img;
 	JDIMENSION y, x, width, height;
-	struct jpeg_decompress_struct cinfo;
-	uint32_t *p, *pixels;
+	uint32_t *p;
 	size_t len;
 
-	pixels = NULL;
-	cinfo.err = jpeg_std_error(&wp_err.mgr);
+	cinfo->err = jpeg_std_error(&wp_err.mgr);
 	wp_err.mgr.error_exit = error_jpg;
 
 	if (setjmp(wp_err.env)) {
 		debug("failed to parse input as (RGB) JPEG\n");
-		jpeg_destroy_decompress(&cinfo);
-		free(pixels);
+		jpeg_destroy_decompress(cinfo);
 		return NULL;
 	}
 
-	jpeg_create_decompress(&cinfo);
-	jpeg_stdio_src(&cinfo, fp);
-	jpeg_read_header(&cinfo, TRUE);
+	jpeg_create_decompress(cinfo);
+	jpeg_stdio_src(cinfo, fp);
+	jpeg_read_header(cinfo, TRUE);
 
-	width = cinfo.image_width;
-	height = cinfo.image_height;
-	cinfo.out_color_space = JCS_EXT_BGRA;
+	width = cinfo->image_width;
+	height = cinfo->image_height;
+	cinfo->out_color_space = JCS_EXT_BGRA;
 
-	jpeg_start_decompress(&cinfo);
+	jpeg_start_decompress(cinfo);
 
-	SAFE_MUL3(len, width, height, sizeof(*pixels));
-	p = pixels = xmalloc(len);
+	SAFE_MUL3(len, width, height, sizeof(**pixels));
+	p = *pixels = xmalloc(len);
 
-	if (cinfo.output_components != 4)
+	if (cinfo->output_components != 4)
 		longjmp(wp_err.env, 1);
 
 	for (y = 0; y < height; y++) {
-		jpeg_read_scanlines(&cinfo, (JSAMPARRAY)&p, 1);
+		jpeg_read_scanlines(cinfo, (JSAMPARRAY)&p, 1);
 		p += width;
 	}
 
-	jpeg_finish_decompress(&cinfo);
-	jpeg_destroy_decompress(&cinfo);
+	jpeg_finish_decompress(cinfo);
+	jpeg_destroy_decompress(cinfo);
 
-	img = pixman_image_create_bits(PIXMAN_a8r8g8b8, width, height, pixels,
+	img = pixman_image_create_bits(PIXMAN_a8r8g8b8, width, height, *pixels,
 	    width * sizeof(uint32_t));
 	if (img == NULL)
 		errx(1, "failed to create pixman image");
 
+	return img;
+}
+
+pixman_image_t *
+load_jpeg(FILE *fp)
+{
+	struct jpeg_decompress_struct cinfo;
+	pixman_image_t *img;
+	uint32_t *pixels;
+
+	pixels = NULL;
+	img = do_load_jpeg(fp, &cinfo, &pixels);
+	if (img == NULL)
+		free(pixels);
 	return img;
 }
